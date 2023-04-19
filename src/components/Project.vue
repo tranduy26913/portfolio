a81a712ec1e01aaca54ebbac8da319e5b2af6562
19110339 <tranbaoduy4@gmail.com>
2023-04-19 13:37:31 +0700

    add: project section

---
+++ src/components/Project.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <section id="project" class="lg:container mx-6 lg:mx-auto mt-8 dark:text-dark">
+        <h1 class="text-center text-[30px] font-semibold">PROJECT</h1>
+        <ul class="flex justify-center flex-wrap items-center gap-x-6 gap-y-7 m-auto w-full min-h-[300px] shadow-neumor rounded-xl
+         py-7 dark:bg-dark dark:shadow-neumor-dark">
+            <!-- <li>
+                <img src="" alt="">
+            </li> -->
+        </ul>
+    </section>
+</template>
--- src/layouts/Home.vue
+++ src/layouts/Home.vue
@@ -3,12 +3,14 @@ import Header from '../components/Header.vue'
 import Footer from '../components/Footer.vue'
 import About from '../components/About.vue'
 import Skill from '../components/Skill.vue'
+import Project from '../components/Project.vue'
 </script>
 
 <template>
     <Header>
     </Header>
     <About></About>
-<Skill></Skill>
-    <Footer ></Footer>
+    <Skill></Skill>
+    <Project></Project>
+    <Footer></Footer>
 </template>
